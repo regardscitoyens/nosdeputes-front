60f17877dab1f938e7617572dd48d49788e4dc39
aaadryyy <adrienquillet@gmail.com>
2023-11-29 23:08:11 +0100

    refactor(dossier): CardLayout primary and secondary variant handling

--- components/folders/CardLayout/index.tsx
+++ components/folders/CardLayout/index.tsx
@@ -2,19 +2,29 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import DeputyPreview from "../DeputyPreview";
 
 interface CardLayoutProps {
-  title: string;
+  title: string | React.ReactNode;
   children: React.ReactNode;
+  variant: "primary" | "secondary";
 }
 
-const CardLayout = ({ title, children }: CardLayoutProps) => {
+const CardLayout = ({ title, children, variant }: CardLayoutProps) => {
+  const cardBackgroundColor = variant === "primary" ? "white" : "grey.50";
+  const cardBoxShadow =
+    variant === "primary"
+      ? "0px 2px 4px 0px rgba(26, 32, 44, 0.08), 0px 1px 2px 0px rgba(26, 32, 44, 0.08)"
+      : "none";
   return (
-    <Card elevation={0} sx={{ backgroundColor: "grey.50" }}>
+    <Card
+      elevation={0}
+      sx={{
+        backgroundColor: cardBackgroundColor,
+        boxShadow: cardBoxShadow,
+      }}
+    >
       <CardContent sx={{ padding: 2 }}>
-        <Typography variant="body1" fontWeight="bold" pb={3}>
+        <Typography variant="body1" fontWeight="regular" pb={3}>
           {title}
         </Typography>
         {children}
