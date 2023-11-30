5bf0b530223d24f7a670494f9e30d928cf9de2d2
alexandre <alex.fauquette@gmail.com>
2023-11-30 22:00:25 +0100

    add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,32 @@ import { NavBar, NavigationItem } from "@/components/NavBar";
 const inter = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  robots: "index, follow",
+  title: "NosDéputés.fr : Observatoire citoyen de l'activité parlementaire",
+  description: "Observatoire citoyen de l'activité parlementaire",
+
+  twitter: {
+    card: "summary",
+    site: "@RegardsCitoyens",
+    title: "NosDéputés.fr par @RegardsCitoyens",
+    description:
+      "Observatoire citoyen de l'activité parlementaire à l'Assemblée nationale",
+    images: {
+      url: "https://www.nosdeputes.fr/images/xneth/bouton_logo.png",
+      type: "image/png",
+    },
+  },
+  openGraph: {
+    type: "website",
+    title: "NosDéputés.fr par Regards Citoyens",
+    siteName: "NosDéputés.fr",
+    description:
+      "Observatoire citoyen de l'activité parlementaire à l'Assemblée nationale",
+    url: "https://www.NosDéputés.fr",
+    locale: "fr_FR",
+    images: "https://www.nosdeputes.fr/images/xneth/bouton_logo.png",
+  },
+};
 
 const navigation: NavigationItem[] = [
   { name: "Home", href: "/" },
