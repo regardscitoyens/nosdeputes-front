50d6dc7c37180cf87af2a3ff2cf0a8a46e50f2ad
Louis Sanna <louis.sanna@ekimetrics.com>
2023-12-09 16:51:18 +0100

    feat(dossier): remove async call to avoid conflict

--- app/dossiers/page.tsx
+++ app/dossiers/page.tsx
@@ -1,14 +1,10 @@
-import { getDossiers } from "@/repository/database";
 import Link from "next/link";
 
 export default async function Dossiers() {
-  const dossiers = await getDossiers();
   // TODO: match old website url ":id/dossiers/:id"
   return (
     <div>
       <Link href="dossiers/dossier">Lien vers un dossier</Link>
-      <h3>Rows from the Dossier Table: </h3>
-      <div>{JSON.stringify(dossiers)}</div>
     </div>
   );
 }
