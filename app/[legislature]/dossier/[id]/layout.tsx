1222719feab69ff3d189b908e7c7ff5cc2cd6704
alexandre <alex.fauquette@gmail.com>
2023-12-15 07:22:38 +0100

    clean naming

--- app/[legislature]/dossier/[id]/layout.tsx
+++ app/[legislature]/dossier/[id]/layout.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-
 import { HeroSection } from "@/components/folders/HeroSection";
-import { PreviewTab } from "@/components/folders/PreviewTab";
-import { DebateTab } from "@/components/folders/DebateTab";
-import { getDossier } from "@/repository/database";
-import Tabs2 from "./Tabs";
+import Tabs from "./Tabs";
 
 export default async function Dossier({ children }: React.PropsWithChildren) {
   return (
     <>
       <HeroSection />
-      <Tabs2 />
+      <Tabs />
       {children}
     </>
   );
